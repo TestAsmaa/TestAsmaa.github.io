90a049cc5d10cd3d12443fa336305201e86dfb8a
Asmaa <60095003@cna-qatar.edu.qa>
2022-04-05 19:47:53 +0300

    btn

--- src/try.js
+++ src/try.js
@@ -1,6 +1,9 @@
 let p = document.getElementById("p1")
 
+btn = document.addEventListener("btn")
+console.log("js file")
 
+btn.addEventListener("click", change)
 function change() {
     p.innerHTML = "<h1>Changed!!!!</h1>"
     console.log("hii")
